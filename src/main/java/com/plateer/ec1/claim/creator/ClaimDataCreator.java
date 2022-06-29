c46b3818ff009eb2ce2f41c7c1e3a11da1920ef6
SPARK <hangangnaru@plateer.com>
2022-06-29 11:07:29 +0900

    수정

--- src/main/java/com/plateer/ec1/claim/creator/ClaimDataCreator.java
+++ src/main/java/com/plateer/ec1/claim/creator/ClaimDataCreator.java
@@ -3,10 +3,10 @@ package com.plateer.ec1.claim.creator;
 import com.plateer.ec1.claim.dto.ClaimDto;
 import com.plateer.ec1.claim.dto.ClaimProcessDto;
 import com.plateer.ec1.claim.enums.ClaimType;
-import com.plateer.ec1.claim.model.OrderBenefit;
-import com.plateer.ec1.claim.model.OrderBenefitRelation;
-import com.plateer.ec1.claim.model.OrderClaim;
-import com.plateer.ec1.claim.model.OrderCost;
+import com.plateer.ec1.common.model.order.OpClmInfo;
+import com.plateer.ec1.common.model.order.OpOrdBnfInfo;
+import com.plateer.ec1.common.model.order.OpOrdBnfRelInfo;
+import com.plateer.ec1.common.model.order.OpOrdCostInfo;
 
 import java.util.List;
 
@@ -20,50 +20,50 @@ abstract public class ClaimDataCreator {
     }
 
     public ClaimProcessDto makeCreateData(ClaimDto dto) {
-        List<OrderClaim> orderClaimList = makeInsertOrderClaim(dto);
-        List<OrderCost> orderCostList = makeInsertOrderCost(dto);
-        List<OrderBenefitRelation> orderBenefitRelationList = makeInsertOrderBenefitRelation(dto);
+        List<OpClmInfo> orderClaimList = makeInsertOrderClaim(dto);
+        List<OpOrdCostInfo> orderCostList = makeInsertOrderCost(dto);
+        List<OpOrdBnfRelInfo> orderBenefitRelationList = makeInsertOrderBenefitRelation(dto);
         return manipulateData(orderClaimList, orderCostList, orderBenefitRelationList, null);
     }
 
     /**
      * 주문클레임 테이블 insert 대상데이터를 만든다
      * */
-    abstract protected List<OrderClaim> makeInsertOrderClaim(ClaimDto dto);
+    abstract protected List<OpClmInfo> makeInsertOrderClaim(ClaimDto dto);
 
     /**
      * 주문비용 테이블 insert 대상데이터를 만든다
      * */
-    abstract protected List<OrderCost> makeInsertOrderCost(ClaimDto dto);
+    abstract protected List<OpOrdCostInfo> makeInsertOrderCost(ClaimDto dto);
 
     /**
      * 주문혜택관계 테이블 insert 대상데이터를 만든다
      * */
-    abstract protected List<OrderBenefitRelation> makeInsertOrderBenefitRelation(ClaimDto dto);
+    abstract protected List<OpOrdBnfRelInfo> makeInsertOrderBenefitRelation(ClaimDto dto);
 
 
     public ClaimProcessDto makeUpdateData(ClaimDto dto) {
-        List<OrderClaim> orderClaimList = makeUpdateOrderClaim(dto);
-        List<OrderCost> orderCostList = makeUpdateOrderCost(dto);
-        List<OrderBenefit> orderBenefitList = makeUpdateOrderBenefit(dto);
+        List<OpClmInfo> orderClaimList = makeUpdateOrderClaim(dto);
+        List<OpOrdCostInfo> orderCostList = makeUpdateOrderCost(dto);
+        List<OpOrdBnfInfo> orderBenefitList = makeUpdateOrderBenefit(dto);
         return manipulateData(orderClaimList, orderCostList, null, orderBenefitList);
     }
 
     /**
      * 주문클레임 테이블 update 대상데이터를 만든다
      * */
-    protected abstract List<OrderClaim> makeUpdateOrderClaim(ClaimDto dto);
+    protected abstract List<OpClmInfo> makeUpdateOrderClaim(ClaimDto dto);
     /**
      * 주문비용 테이블 update 대상데이터를 만든다
      * */
-    protected abstract List<OrderCost> makeUpdateOrderCost(ClaimDto dto);
+    protected abstract List<OpOrdCostInfo> makeUpdateOrderCost(ClaimDto dto);
     /**
      * 주문혜택 테이블 update 대상데이터를 만든다
      * */
-    protected abstract List<OrderBenefit> makeUpdateOrderBenefit(ClaimDto dto);
+    protected abstract List<OpOrdBnfInfo> makeUpdateOrderBenefit(ClaimDto dto);
 
 
-    private ClaimProcessDto manipulateData(List<OrderClaim> orderClaimList, List<OrderCost> orderCostList, List<OrderBenefitRelation> orderBenefitRelationList, List<OrderBenefit> orderBenefitList) {
+    private ClaimProcessDto manipulateData(List<OpClmInfo> orderClaimList, List<OpOrdCostInfo> orderCostList, List<OpOrdBnfRelInfo> orderBenefitRelationList, List<OpOrdBnfInfo> orderBenefitList) {
         ClaimProcessDto claimProcessDto = new ClaimProcessDto();
         claimProcessDto.setOrderClaimList(orderClaimList);
         claimProcessDto.setOrderCostList(orderCostList);
