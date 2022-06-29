4a9327f38b5f9a43582185e3e61bf4fd80c42fad
SPARK <hangangnaru@plateer.com>
2022-06-29 11:06:24 +0900

    변경된 모델로 치환

--- src/main/java/com/plateer/ec1/claim/creator/GeneralCancelDataCreator.java
+++ src/main/java/com/plateer/ec1/claim/creator/GeneralCancelDataCreator.java
@@ -2,10 +2,10 @@ package com.plateer.ec1.claim.creator;
 
 import com.plateer.ec1.claim.dto.ClaimDto;
 import com.plateer.ec1.claim.enums.ClaimType;
-import com.plateer.ec1.claim.model.OrderBenefit;
-import com.plateer.ec1.claim.model.OrderBenefitRelation;
-import com.plateer.ec1.claim.model.OrderClaim;
-import com.plateer.ec1.claim.model.OrderCost;
+import com.plateer.ec1.common.model.order.OpClmInfo;
+import com.plateer.ec1.common.model.order.OpOrdBnfInfo;
+import com.plateer.ec1.common.model.order.OpOrdBnfRelInfo;
+import com.plateer.ec1.common.model.order.OpOrdCostInfo;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -19,33 +19,33 @@ public class GeneralCancelDataCreator extends ClaimDataCreator{
     }
 
     @Override
-    protected List<OrderClaim> makeInsertOrderClaim(ClaimDto dto) {
+    protected List<OpClmInfo> makeInsertOrderClaim(ClaimDto dto) {
         System.out.println("일반상품 취소 클레임 데이터 만든다");
         return null;
     }
 
     @Override
-    protected List<OrderCost> makeInsertOrderCost(ClaimDto dto) {
+    protected List<OpOrdCostInfo> makeInsertOrderCost(ClaimDto dto) {
         return null;
     }
 
     @Override
-    protected List<OrderBenefitRelation> makeInsertOrderBenefitRelation(ClaimDto dto) {
+    protected List<OpOrdBnfRelInfo> makeInsertOrderBenefitRelation(ClaimDto dto) {
         return null;
     }
 
     @Override
-    protected List<OrderClaim> makeUpdateOrderClaim(ClaimDto dto) {
+    protected List<OpClmInfo> makeUpdateOrderClaim(ClaimDto dto) {
         return null;
     }
 
     @Override
-    protected List<OrderCost> makeUpdateOrderCost(ClaimDto dto) {
+    protected List<OpOrdCostInfo> makeUpdateOrderCost(ClaimDto dto) {
         return null;
     }
 
     @Override
-    protected List<OrderBenefit> makeUpdateOrderBenefit(ClaimDto dto) {
+    protected List<OpOrdBnfInfo> makeUpdateOrderBenefit(ClaimDto dto) {
         return null;
     }
 }
